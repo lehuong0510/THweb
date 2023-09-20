5be3cd17a7b1c6a030993a8ec97e594452d2b30d
Le Huong <le308934@gmail.com>
2023-09-20 15:06:56 +0700

    addcode

---
+++ Models/MenuItem.cs
@@ -0,0 +1,9 @@
+﻿namespace _13_09web_student.Models
+{
+    public class MenuItem
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Link { get; set; }
+    }
+}
---
+++ ViewComponents/RenderViewComponent.cs
@@ -0,0 +1,25 @@
+﻿using _13_09web_student.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace _13_09web_student.ViewComponents
+{
+    public class RenderViewComponent : ViewComponent
+    {
+        private List<MenuItem> menuItems = new List<MenuItem>();
+        public RenderViewComponent()
+        {
+           menuItems = new List<MenuItem>()
+             {
+                new MenuItem(){ Id =1, Name="Branches", Link="Branches/List"},
+                new MenuItem(){ Id =2, Name="Students", Link="Students/List"},
+                new MenuItem(){ Id =3, Name="Subjects", Link="Subjects/List"},
+                new MenuItem(){ Id =4, Name="Courses", Link="Courses/List"}
+            };
+        }
+        public async Task<IViewComponentResult> InvoleAsync()//hien thi view theo co che bat dong bo
+        {
+            return View("RenderLeftMenu", menuItems);
+        }
+    }
+
+}
